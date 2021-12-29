f7a0b4ff6fa457185bead5aa2b972d642f5e00aa
Joanna <m.joanna.foss@gmail.com>
2021-12-29 00:10:43 -0600

    in-prog: header and nav button

--- js/script.js
+++ js/script.js
@@ -5,3 +5,53 @@ $(window).resize(function() {
 });
 
 $(window).trigger('resize');
+
+function headerText(arrOfText, id, arrOfColors){
+    if(arrOfColors === undefined){
+        arrOfColors = ['#000'];
+    }
+    let visible = true;
+    let underscore = document.getElementById('underscore');
+    let letterCount = 1;
+    let x = 1;
+    let waiting = false;
+    let target = document.getElementById(id);
+    target.setAttribute('style', 'color:' + arrOfColors[0]);
+    window.setInterval(()=>{
+        if(letterCount === 0 && waiting === false){
+            waiting = true;
+            target.innerHTML = arrOfText[0].substring(0, letterCount);
+            window.setTimeout(()=>{
+                let usedColor = arrOfColors.shift();
+                arrOfColors.push(usedColor);
+                let usedText = arrOfText.shift();
+                arrOfText.push(usedText);
+                x = 1;
+                target.setAttribute('style', 'color:' + arrOfColors[0]);
+                letterCount += x;
+                waiting = false;
+            }, 1000);
+        } else if (letterCount === arrOfText[0].length + 1 && waiting === false){
+            waiting = true;
+            window.setTimeout(()=>{
+                x = -1;
+                letterCount += x;
+                waiting = false;
+            }, 1000);
+        } else if (waiting === false){
+            target.innerHTML = arrOfText[0].substring(0, letterCount);
+            letterCount += x;
+        }
+    }, 125);
+    window.setInterval(()=>{
+        if(visible === true){
+            underscore.className = 'underscore hidden';
+            visible = false;
+        } else {
+            underscore.className = 'underscore';
+            visible = true;
+        }
+    }, 400);
+}
+
+headerText(["A software developer.", "A USAF veteran.", "A chess enthusiast."], "header-text");
